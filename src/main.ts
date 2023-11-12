d379e3c14855935f8efb81345c91f88b46f8615a
Md. Shofiul Islam <ytshofiul@gmail.com>
2023-11-12 20:59:18 +0600

    helmet added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(helmet())
+  await app.listen(3005);
 }
 bootstrap();
